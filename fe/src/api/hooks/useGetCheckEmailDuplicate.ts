4b1ef6488a741b67814073aa55aaff78ae0a66e1
DESKTOP-URKE2VL\SSAFY <godqhr827@naver.com>
2026-03-06 09:49:31 +0900

    [S14P21D105-32] refactor: update signup api format

---
+++ fe/src/api/hooks/useGetCheckEmailDuplicate.ts
@@ -0,0 +1,19 @@
+import { useMutation } from '@tanstack/react-query';
+
+import { getFetchInstance } from '../instance';
+
+export const getCheckEmailDuplicatePath = () => `/members/check-email`;
+
+export const checkEmailDuplicate = async (email: string) => {
+  const response = await getFetchInstance().get<{ isDuplicated: boolean }>(
+    getCheckEmailDuplicatePath(),
+    { params: { email } },
+  );
+  return response.data;
+};
+
+export const useCheckEmailDuplicate = () => {
+  return useMutation({
+    mutationFn: (email: string) => checkEmailDuplicate(email),
+  });
+};
---
+++ fe/src/api/hooks/usePostRequestSmsCode.ts
@@ -0,0 +1,19 @@
+import { useMutation } from '@tanstack/react-query';
+
+import { getFetchInstance } from '../instance';
+
+export const getRequestSmsCodePath = () => `/auth/sms/send`;
+
+export const requestSmsCode = async (phone: string) => {
+  const response = await getFetchInstance().post<{ expireAt: string }>(
+    getRequestSmsCodePath(),
+    { phone },
+  );
+  return response.data;
+};
+
+export const useRequestSmsCode = () => {
+  return useMutation({
+    mutationFn: (phone: string) => requestSmsCode(phone),
+  });
+};
